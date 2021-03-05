31967c5dc9f8b19cb13f06e30aed1bfcba86b700
Enock8574 <sentry.ec@gmail.com>
2021-03-05 12:50:48 -0500

    Agregamos Funcionalidades a los botones y ahora es posible encriptar y desencriptar

--- EncryptMD5/EncryptMD5/Form1.Designer.cs
+++ EncryptMD5/EncryptMD5/Form1.Designer.cs
@@ -31,94 +31,97 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 69);
+            this.label1.Location = new System.Drawing.Point(12, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Valor";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 94);
+            this.label2.Location = new System.Drawing.Point(12, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Valor Encriptado:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 120);
+            this.label3.Location = new System.Drawing.Point(12, 109);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(106, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Valor Desencriptado:";
             // 
-            // textBox1
+            // textBoxValue
             // 
-            this.textBox1.Location = new System.Drawing.Point(84, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.textBoxValue.Location = new System.Drawing.Point(137, 57);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(293, 20);
+            this.textBoxValue.TabIndex = 3;
             // 
-            // textBox2
+            // textBoxEncrypt
             // 
-            this.textBox2.Location = new System.Drawing.Point(84, 92);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBoxEncrypt.Location = new System.Drawing.Point(137, 81);
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(293, 20);
+            this.textBoxEncrypt.TabIndex = 4;
             // 
-            // textBox3
+            // textBoxDecrypt
             // 
-            this.textBox3.Location = new System.Drawing.Point(84, 118);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 5;
+            this.textBoxDecrypt.Location = new System.Drawing.Point(137, 106);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(293, 20);
+            this.textBoxDecrypt.TabIndex = 5;
             // 
-            // button1
+            // btnEncrypt
             // 
-            this.button1.Location = new System.Drawing.Point(190, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Location = new System.Drawing.Point(255, 132);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(89, 23);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encriptar";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
             // 
-            // button2
+            // btnDecrypt
             // 
-            this.button2.Location = new System.Drawing.Point(190, 116);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Location = new System.Drawing.Point(350, 132);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(80, 23);
+            this.btnDecrypt.TabIndex = 7;
+            this.btnDecrypt.Text = "Desencriptar";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(460, 190);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.textBoxValue);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "EncryptMD5";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,11 +132,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
     }
 }
 
--- EncryptMD5/EncryptMD5/Form1.cs
+++ EncryptMD5/EncryptMD5/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,37 @@ namespace EncryptMD5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string hash = "Valores";
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(textBoxValue.Text);
+            using(MD5CryptoServiceProvider md5=new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using(TripleDESCryptoServiceProvider tripleDES=new TripleDESCryptoServiceProvider() { Key=keys,Mode=CipherMode.ECB,Padding=PaddingMode.PKCS7})
+                {
+                    ICryptoTransform transform = tripleDES.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    textBoxEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = Convert.FromBase64String(textBoxEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripleDES.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    textBoxDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
         }
     }
 }
